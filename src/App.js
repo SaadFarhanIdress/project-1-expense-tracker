80f422cb46519461e709aa9bf9adbbbf3bd2d02f
Saad Farhan <sunehrafarhan123@gmail.com>
2021-01-19 06:05:24 -0800

    Created functional components, remove ids, replace class with className and for with htmlFor, used all the components in App.js, write css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Header } from "./components/Header";
+import './App.css';
+import { Balance } from "./components/Balance";
+import { IncomeExpense } from "./components/IncomeExpense";
+import { TransactionHistory } from "./components/TransactionHistory";
+
 function App() {
   return (
     <div>
-      
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionHistory />
+      </div>
     </div>
   );
 }
