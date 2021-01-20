eb928ea2208605151a1aaee65d180d700ee9f18d
Saad Farhan <sunehrafarhan123@gmail.com>
2021-01-20 03:40:44 -0800

    Update IncomeExpense.js
    Imported neccessary things.
    Imported transactions from GlobalContext using useContext,
    Created amounts const that have all the amounts,
    Created income const and it have the values that are more than 0 and also they all are added,
    Created expense const and it have the values that are less than 0 and also they all are added,
    Printed income and expense and used .toFixed() in them.

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpense = () => {
+    const { transactions } = useContext(GlobalContext);
+    const amounts = transactions.map(transaction => transaction.amount)
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+        -1
+    )
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p className="money plus">+$0.00</p>
+                <p className="money plus">+${parseFloat(income).toFixed(2)}</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p className="money minus">-$0.00</p>
+                <p className="money minus">-${parseFloat(expense).toFixed(2)}</p>
             </div>
         </div>
     )
