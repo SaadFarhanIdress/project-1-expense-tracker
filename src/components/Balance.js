35c85a43d0967ef603d21346be038699c0bd1647
Saad Farhan <sunehrafarhan123@gmail.com>
2021-01-26 14:18:59 +0500

    Update Balance.js

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -7,7 +7,7 @@ export const Balance = () => {
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
     return (
         <>
-            <h4>Your Balance</h4>
+            <h4>Your Balane</h4>
             <h1>${total}</h1>
         </>
     )
